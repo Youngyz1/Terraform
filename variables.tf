49024ef8708a268c173213e53f52723ce7973005
youngyz1 <ofiliyoungyz@gmail.com>
2025-10-28 23:36:47 +0000

    Update .gitignore to ignore Terraform state and local files

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
+
+variable "region" {
+}
+
+variable "main_vpc" {
+  default = "main_vpc"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
