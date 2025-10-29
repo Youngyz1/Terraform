3cdbf53e4d04779901e2dd9ec8f1a47302e4b1fd
youngyz1 <ofiliyoungyz@gmail.com>
2025-10-29 01:17:06 +0000

    updated

--- variables.tf
+++ variables.tf
@@ -15,17 +15,6 @@
 #   default = "10.0.0.0/16"
 # }
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
 
 variable "region" {
   description = "AWS Region"
