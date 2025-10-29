f1ba24ec3567785c9a1e8185d4f0383291d0c767
youngyz1 <ofiliyoungyz@gmail.com>
2025-10-29 01:20:58 +0000

    update

--- 00-provider.tf
+++ 00-provider.tf
@@ -1,22 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "6.18.0"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region = var.region
-# }
-
-# variable "region" {
-#   default = "us-east-1"
-# }
-
-# # Configuration options
-
 terraform {
   required_providers {
     aws = {
@@ -27,6 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.region
-  
+  region = var.region
 }
