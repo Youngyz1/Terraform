648406d4c935dcb5f107a6e370c510f27fff39cc
youngyz1 <ofiliyoungyz@gmail.com>
2025-10-29 10:21:49 +0000

    updated

--- variables.tf
+++ variables.tf
@@ -1,10 +1,13 @@
 # variable "aws_access_key" {
+#    default = ""
 # }
 
 # variable "aws_secret_key" {
+#   default = ""
 # }
 
 # variable "region" {
+#   default = "us-east-1"
 # }
 
 # variable "main_vpc" {
